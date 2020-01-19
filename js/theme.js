9eff9838f7be9123a92fe3af62292931d4ec1968
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-01-20 02:58:59 +0530

    updating form

--- js/theme.js
+++ js/theme.js
@@ -25,11 +25,24 @@
      * submitted the form!
      * --------------------------------------------------------
      */
-    $("#contactForm").submit(function(e) {
+    let testForm = document.querySelector("#contactForm");
+
+    testForm.addEventListener('submit', e => {
         e.preventDefault();
-        var $form = $(this);
-        $.post($form.attr("action"), $form.serialize()).then(function() {
-            alert("Thank you!");
-        });
+
+        const formData = new FormData(testForm);
+        fetch(testForm.getAttribute('action'), {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/x-www-form-urlencoded;charset=UTF-8',
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+                },
+                body: new URLSearchParams(formData).toString()
+            })
+            .then(res => {
+                if (res) {
+                    alert('Thank for your submission!');
+                }
+            });
     });
 })(jQuery);
